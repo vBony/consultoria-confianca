a745a5d13a9e19c8c375f9137175e25a484f2e1a
MATHEUS <matheus8142@hotmail.com>
2021-10-05 21:19:52 -0300

    (#6) - Adicionando sessão de contato

--- app/views/home.php
+++ app/views/home.php
@@ -313,6 +313,66 @@
             </div>
         </div>
     </section>
+
+    <section class="contact">
+        <div class="container">
+            <div class="section-title">
+                <h2>Contato</h2>
+                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="info-box">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <h3>Endereço</h3>
+                                <p>Goiânia - GO</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box mt-4">
+                                <i class="fas fa-envelope"></i>
+                                <h3>Nosso e-mail</h3>
+                                <p>info@example.com <br> contact@example.com</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="info-box mt-4">
+                                <i class="fas fa-phone-alt"></i>
+                                <h3>Telefone</h3>
+                                <p>+1 5589 55488 55 <br> +1 6678 254445 41</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <form method="POST" role="form" class="php-email-form">
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <input type="text" class="f-color form-control" placeholder="Nome" required>
+                            </div>
+                            <div class="col-md-6 form-group mt-3 mt-md-0">
+                                <input type="email" class="f-color form-control" placeholder="E-mail" required>
+                            </div>
+                        </div>
+                        <div class="form-group mt-3">
+                            <input type="text" class="f-color form-control" placeholder="Assunto" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <textarea class="f-color textarea form-control" name="message" rows="5" placeholder="Menssagem" required></textarea>
+                        </div>
+                        <div class="my-3">
+                            <div class="loading">Carregando</div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
     
 </main>
 
