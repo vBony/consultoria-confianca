aca4bda211ac5d3fe9d58564ab6f96514a137246
VInicius Ferreira <contato.vinicius-af@outlook.com>
2022-02-15 20:41:03 -0300

    layout simulação - passo 1

--- app/views/financiamento.php
+++ app/views/financiamento.php
@@ -22,6 +22,91 @@
                         <h1 class="default-title"><strong> Consultoria Confiança </strong></h1>
                     </div>
                 </div>
+
+                <div class="card">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-4 col-sm-12 d-flex" id="img-area">
+                            <img class="img img-fluid" src="<?=$baseUrl?>app/assets/imgs/app/simulation.jpg" alt="">
+                        </div>
+
+                        <div class="col-lg-7 col-md-12 col-sm-12 p-4">
+                            <div>
+                                <div class="mb-4">
+                                    <div class="progress" id="progress-financiamento">
+                                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="text-muted mt-1" id="breadcrumb">Passo 1 de 2 - Sobre o imóvel</div>
+                                    </div>
+                                </div>
+                                <h3 class="default-title">Dados para a simulação</h3>
+                                <p class="text-secondary">Para começar, conta pra gente algumas informações para calcularmos seu financiamento.</p>
+                            </div>
+
+                            <div class="mt-4">
+                                <form id="step-1">
+                                    <div class="row mb-2">
+                                        <div class="form-group">
+                                            <div class="input-label">Tipo de imóvel</div>
+                                            <select class="form-control default-input">
+                                                <option>Selecione</option>
+                                                <option>1</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                            <div class="invalid-msg"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="form-group">
+                                            <div class="input-label">Estado do imóvel</div>
+                                            <select class="form-control default-input">
+                                                <option>Selecione</option>
+                                                <option>1</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>
+                                            <div class="invalid-msg"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="form-group">
+                                            <div class="input-label">Valor do Imóvel</div>
+                                            <input type="text" class="form-control default-input" placeholder="R$ 0,00">
+                                            <div class="invalid-msg"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="form-group">
+                                            <div class="input-label">Valor pretendido de financiamento</div>
+                                            <input type="text" class="form-control default-input" placeholder="R$ 0,00">
+                                            <div class="invalid-msg"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mb-2">
+                                        <div class="form-group">
+                                            <div class="input-label">Prazo em meses</div>
+                                            <input type="text" class="form-control default-input" placeholder="0">
+                                            <div class="invalid-msg"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="form-group" id="btn-area">
+                                            <button class="default-button" id="btn-submit">Continuar</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
