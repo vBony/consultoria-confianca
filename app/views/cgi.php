f5bbc53966a8a65b4804fbd9a43d3cf6a3af546b
VInicius Ferreira <contato.vinicius-af@outlook.com>
2022-05-31 19:31:18 -0300

    criação de arquivos iniciais

---
+++ app/views/cgi.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./app/assets/libraries/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./app/assets/css/cgi.css">
+    <link rel="stylesheet" href="./app/assets/css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="./app/assets/js/cgi.js"></script>
+    <script src="./app/assets/libraries/glide-3.4.1/dist/glide.min.js"></script>
+    <link rel="stylesheet" href="./app/assets/libraries/glide-3.4.1/dist/css/glide.core.min.css">
+    <link rel="stylesheet" href="./app/assets/libraries/glide-3.4.1/dist/css/glide.theme.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
+    <title>Confiança Consultoria em Crédito Imobiliário Eireli</title>
+
+    <meta name="description" content="Financie seu imóvel com as melhores taxas de juros do mercado. Solicite uma simulação personalizada e vamos encontrar juntos as melhores opções para você.">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?= $baseUrl ?>">
+    <meta property="og:title" content="Confiança Consultoria em Crédito Imobiliário Eireli">
+    <meta property="og:description" content="Financie seu imóvel com as melhores taxas de juros do mercado. Solicite uma simulação personalizada e vamos encontrar juntos as melhores opções para você, com parcelas que cabem no seu bolso.">
+    <meta property="og:image" content="<?=$baseUrl?>app/assets/imgs/app/thumb/thumb-default.jpg">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="<?=$baseUrl?>app/assets/imgs/app/thumb/thumb-default.jpg">
+    <meta property="twitter:url" content="<?= $baseUrl ?>">
+    <meta property="twitter:title" content="Confiança Consultoria em Crédito Imobiliário Eireli">
+    <meta property="twitter:description" content="Financie seu imóvel com as melhores taxas de juros do mercado. Solicite uma simulação personalizada e vamos encontrar juntos as melhores opções para você, com parcelas que cabem no seu bolso.">
+    <meta property="twitter:image" content="<?=$baseUrl?>app/assets/imgs/app/thumb/thumb-default.jpg">
+
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="Portuguese">
+
+    <meta name="google-site-verification" content="P5MwvEL7Gey2HwwsmrvPWOvXdUWSauFNRB2C1PIdwaM" />
+    <link rel="icon" type="image/png" href="<?=$baseUrl?>app/assets/imgs/app/logo-black.png">
+
+    <meta name="robots" content="noindex, nofollow">
+</head>
+<body>
+    <h1>cgi</h1>
+</body>
+</html>
